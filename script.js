b2203d01c9de3ddce687bb57a95c472fa2f4c308
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-01-26 10:57:30 +0100

    implemented clear button

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-let canvas = document.querySelector("canvas");
-let form = document.querySelector("form");
+let form;
+
+let canvas;
 let canvasCenterX;
 let canvasCenterY;
+let context
 
 function init(){
+    form = document.querySelector("form");
+    canvas = document.querySelector("canvas");
+    
     canvasCenterX = canvas.width/2;
     canvasCenterY = canvas.height/2
-    console.log("Center: \n\rx: " + canvasCenterX + "  y: " + canvasCenterY)
+    //console.log("Center: \n\rx: " + canvasCenterX + "  y: " + canvasCenterY)
+
+    /**@type CanvasRenderingContext2D*/
+    context = canvas.getContext("2d");
     
     form.clear.onclick = clear;
-    form.trapezoid
+    form.trapezoid.onclick = trapezoid;
+    form.diamond.onclick = diamond;
+    form.zigzag.onclick = zigzag;
+    form.spiral.onclick = spiral;
+    form.star.onclick = star;
 }
 
 function clear(){
     console.log("clearing")
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function trapezoid(){
+    console.log("trapezoid")
+    context.
+}
 
+function diamond(){
+    console.log("diamond")
 }
 
 function zigzag(){
-
+    console.log("zigzag")
 }
 
 function spiral(){
-
+    console.log("spiral")
 }
 
 function star(){
-
+    console.log("star")
 }
 
 window.onload = init;
