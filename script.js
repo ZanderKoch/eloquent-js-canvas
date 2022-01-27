c213b29cfbbfda7e29cf543a94290357848ff699
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-01-27 14:35:52 +0100

    implemented zigzag()

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let form;
 
 let canvas;
-let canvasCenterX;
-let canvasCenterY;
-let context
+let centerX;
+let centerY;
+let shortSideLength;
+let margin;
+let context;
+
 
 function init(){
     form = document.querySelector("form");
     canvas = document.querySelector("canvas");
     
-    canvasCenterX = canvas.width/2;
-    canvasCenterY = canvas.height/2
-    //console.log("Center: \n\rx: " + canvasCenterX + "  y: " + canvasCenterY)
+    centerX = canvas.width/2;
+    centerY = canvas.height/2
+    //console.log("Center: \n\rx: " + canvasCenterX + "  y: " + canvasCenterY);
+    shortSideLength = Math.min(canvas.width, canvas.height);
+    margin = shortSideLength / 8;
 
-    /**@type CanvasRenderingContext2D*/
     context = canvas.getContext("2d");
     
     form.clear.onclick = clear;
@@ -25,29 +29,59 @@ function init(){
 }
 
 function clear(){
-    console.log("clearing")
+    console.log("clearing");
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function trapezoid(){
-    console.log("trapezoid")
-    context.
+    console.log("trapezoid");
+    
+    context.beginPath();
+    context.moveTo(200, 100);
+    context.lineTo(canvas.width - 200, 100);
+    context.lineTo(canvas.width - 100, canvas.height - 100);
+    context.lineTo(100, canvas.height - 100);
+    context.closePath();
+    context.stroke();
 }
 
 function diamond(){
-    console.log("diamond")
+    console.log("diamond");
+
+    let cornerDistance = shortSideLength / 2 - margin;
+    
+    context.beginPath()
+    context.moveTo(centerX, centerY - cornerDistance);
+    context.lineTo(centerX + cornerDistance, centerY);
+    context.lineTo(centerX, centerY + cornerDistance);
+    context.lineTo(centerX - cornerDistance, centerY);
+    context.fillStyle = "red";
+    context.fill();
 }
 
 function zigzag(){
-    console.log("zigzag")
+    console.log("zigzag");
+    
+    context.beginPath();
+    context.moveTo(margin, margin);
+    for(cycle = 1; cycle * margin <= canvas.height - margin; cycle++){
+        console.log("yeah");
+        if(cycle % 2 == 0){
+            context.lineTo(margin, margin * cycle)
+        }
+        else{
+            context.lineTo(canvas.width - margin, margin * cycle)
+        }
+    }
+    context.stroke();
 }
 
 function spiral(){
-    console.log("spiral")
+    console.log("spiral");
 }
 
 function star(){
-    console.log("star")
+    console.log("star");
 }
 
 window.onload = init;
