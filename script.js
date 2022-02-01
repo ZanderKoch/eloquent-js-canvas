be16f6819ea8868d0eebc3df822041aae5f32b7c
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-01 12:35:10 +0100

    yeah

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ function init(){
     form.zigzag.onclick = zigzag;
     form.spiral.onclick = spiral;
     form.star.onclick = star;
+    form.house.onclick = house;
+
+    //debug
+    house();
 }
 
 function clear(){
@@ -37,6 +41,8 @@ function trapezoid(){
     console.log("trapezoid");
     
     context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
     context.moveTo(200, 100);
     context.lineTo(canvas.width - 200, 100);
     context.lineTo(canvas.width - 100, canvas.height - 100);
@@ -61,8 +67,10 @@ function diamond(){
 
 function zigzag(){
     console.log("zigzag");
-    
+
     context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
     context.moveTo(margin, margin);
     for(cycle = 1; cycle * margin <= canvas.height - margin; cycle++){
         console.log("yeah");
@@ -78,10 +86,114 @@ function zigzag(){
 
 function spiral(){
     console.log("spiral");
+
+    let segments = 1000;
+    let maxRadius = shortSideLength/2 - margin;
+    console.log("maxRadius = " + maxRadius)
+
+    context.translate(centerX, centerY);
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    for(let i =1; i <= segments; i++){
+        context.rotate((Math.PI * 6) / segments);
+        context.lineTo(( maxRadius/ segments) * i, 0);
+        
+    }
+    context.stroke();
+    context.resetTransform();
 }
 
 function star(){
     console.log("star");
+
+    let points = 5;
+    let rotationStepSize = 2 * Math.PI / points;
+    let angle = 0;
+
+    let majorRadius = shortSideLength / 2 - margin;
+    let minorRadius = majorRadius / 3;
+    context.translate(centerX, centerY);
+    context.beginPath();
+    
+    context.moveTo(majorRadius * Math.cos(angle),
+                   majorRadius * Math.sin(angle));
+    for(angle; angle < Math.PI * 2; angle += rotationStepSize){
+        context.quadraticCurveTo(
+            minorRadius * Math.cos(angle + rotationStepSize / 2),
+            minorRadius * Math.sin(angle + rotationStepSize / 2),
+            majorRadius * Math.cos(angle + rotationStepSize),
+            majorRadius * Math.sin(angle + rotationStepSize)
+        );
+    }
+    context.fillStyle = "yellow";
+    context.fill();
+    context.resetTransform();
+}
+
+function house(){
+    //Sky
+    context.fillStyle = "skyblue";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    //ground
+    context.fillStyle = "darkgreen";
+    context.fillRect(0,canvas.height - 200, canvas.width, 200);
+
+    //clouds
+    
+    context.beginPath();
+    context.fillStyle = "white";
+    context.moveTo(200,300);
+    context.arc(200, 300, 30, 0, 2 * Math.PI);
+    context.arc(260, 300, 50, 0, 2 * Math.PI);
+    context.arc(300, 320, 45, 0, 2 * Math.PI);
+
+    context.moveTo(700, 300);
+    context.arc(700, 300, 40, 0, 2 * Math.PI);
+    context.arc(745, 300, 55, 0, 2 * Math.PI);
+    context.arc(810, 320, 50, 0, 2 * Math.PI);
+    context.fill();
+
+    //house
+    context.beginPath();
+    context.fillStyle = "red";
+    context.moveTo(350, canvas.height - 100);
+    context.lineTo(350, canvas.height - 400);
+    context.lineTo(550, canvas.height - 500);
+    context.lineTo(750, canvas.height - 400);
+    context.lineTo(750, canvas.height - 100);
+    context.fill();
+
+    //house border
+    context.beginPath();
+    context.strokeStyle = "white";
+    context.lineWidth = 10
+    context.moveTo(350, canvas.height - 100);
+    context.lineTo(350, canvas.height - 400);
+    context.lineTo(550, canvas.height - 500);
+    context.lineTo(750, canvas.height - 400);
+    context.lineTo(750, canvas.height - 100);
+    context.stroke();
+
+    //roof
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 10
+    context.lineTo(350, canvas.height - 400);
+    context.lineTo(550, canvas.height - 500);
+    context.lineTo(750, canvas.height - 400);
+    context.stroke();
+
+    //door
+    context.beginPath();
+    context.fillStyle = "saddlebrown";
+    context.fillRect(400, canvas.height - 225, 75, 125)
+
+    //text
+    context.fillStyle = "white"
+    context.font = "12px Georgia"
+    context.fillText("jag klarar inte att göra mer canvas")
 }
 
 window.onload = init;
